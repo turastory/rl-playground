34b4a230d61ef373f0cae48b87d312fd8f54b832
tura <soldier4443@gmail.com>
2018-12-05 22:01:05 +0900

    Add "mastering the game" concept / refactor most of observation tasks

--- frozen-lake/dummy-q.py
+++ frozen-lake/dummy-q.py
@@ -1,9 +1,5 @@
 import gym
 from gym.envs.registration import register
-import numpy as np
-import random as pr
-import matplotlib
-import matplotlib.pyplot as plt
 from util import *
 
 # Register our own environment
@@ -16,20 +12,12 @@ register(
 env = gym.make('FrozenLake-v3')
 env.render()
 
-def rargmax(vector):
-    m = np.amax(vector)
-    # False is interpreted as 0, so this statement means
-    # "Find indices whose value is equal to `m`"
-    indices = np.nonzero(vector == m)[0]
-    return pr.choice(indices)
-
 # Define Q
 Q = np.zeros([env.observation_space.n, env.action_space.n])
-num_episodes = 500
-eval_bucket = 100
 
-rList = []
-for i in range(num_episodes):
+observer = LearningObserver()
+
+for i in range(observer.num_episodes):
     state = env.reset()
     rAll = 0  # Total sum of rewards
     done = False
@@ -46,18 +34,17 @@ for i in range(num_episodes):
 
         rAll += reward
         state = new_state
-    
-    rList.append(rAll)
-    
-    # Calculate local success rate.
-    if i > 0 and i % eval_bucket == 0:
-        local_success_rate = sum(rList[i-eval_bucket:i]) / eval_bucket
-        print("Success rate for {} steps: {}".format(eval_bucket, local_success_rate))
 
-print("Success Rate: " + str(sum(rList) / num_episodes))
+    observer.record(rAll)
+
+    # How fast the model mastered the game
+    observer.check_mastered(i)
+
+    # Calculate local success rate.
+    observer.observe_local_success_rate(i)
+
 print("Final Q-Table values")
 print("LEFT DOWN RIGHT UP")
 print(Q)
 
-plt.bar(range(len(rList)), rList, color="blue")
-plt.show()
+observer.show_barchart()
--- frozen-lake/util.py
+++ frozen-lake/util.py
@@ -1,9 +1,20 @@
 # Some utilities for frozen-lake game.
+import numpy as np
+import random as pr
+import matplotlib.pyplot as plt
 
 LEFT = 0
 DOWN = 1
 RIGHT = 2
 UP = 3
+
+
+def rargmax(vector):
+    m = np.amax(vector)
+    # False is interpreted as 0, so this statement means
+    # "Find indices whose value is equal to `m`"
+    indices = np.nonzero(vector == m)[0]
+    return pr.choice(indices)
 
 
 def interpret_action(action):
@@ -17,3 +28,47 @@ def interpret_action(action):
         return "UP"
     else:
         return "UNKNOWN"
+
+
+def show_barchart(showingList):
+    plt.bar(range(len(showingList)), showingList, color="blue")
+    plt.show()
+
+
+class LearningObserver:
+    def __init__(self):
+        self.reset()
+
+    def reset(self, ):
+        self.reward_list = []
+        self.mastered = False
+        self.master_continuous_success = 10
+        self.master_threshold = 0.95
+        self.num_episodes = 500
+        self.num_local_eval_episodes = 50
+
+    def record(self, reward):
+        self.reward_list.append(reward)
+
+    def observe_local_success_rate(self, step):
+        if step > 0 and step % self.num_local_eval_episodes == 0:
+            local_success_rate = sum(
+                self.reward_list[step - self.num_local_eval_episodes:step]) / self.num_local_eval_episodes
+            print("Success rate for {} steps: {}".format(
+                self.num_local_eval_episodes, local_success_rate))
+
+    def observe_total_success_rate(self):
+        print("Success Rate: " + str(sum(self.reward_list) / self.num_episodes))
+
+    def check_mastered(self, step):
+        def isMastered(item):
+            return item > self.master_threshold
+
+        if not self.mastered and step >= self.master_continuous_success:
+            start = step - self.master_continuous_success
+            if all(map(isMastered, self.reward_list[start:step])):
+                self.mastered = True
+                print("Mastered the game at step {}".format(step))
+
+    def show_barchart(self):
+        show_barchart(self.reward_list)
